e1adb321156c79bb864c74089cbe19903be2ba72
Vic-C87 <102956878+Vic-C87@users.noreply.github.com>
2024-06-20 01:54:51 +0200

    mainmenu

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
     [SerializeField] Sprite myCursor;
-
     [SerializeField] Image myCursorGameObject;
+    [SerializeField] GameObject myLevelSelectScreen;
+    [SerializeField] GameObject myMainScreen;
 
+
+    int myUnlockedLevelIndex;
 
     private void Awake()
     {
@@ -21,7 +25,9 @@ public class GameManager : MonoBehaviour
         else
         {
             Instance = this;
+            DontDestroyOnLoad(this.gameObject);
         }
+        myUnlockedLevelIndex = 1;
     }
 
     // Start is called before the first frame update
@@ -29,6 +35,7 @@ public class GameManager : MonoBehaviour
     {
         Cursor.visible = false;
         myCursorGameObject.sprite = myCursor;
+        myLevelSelectScreen.SetActive(false);
     }
 
     // Update is called once per frame
@@ -40,5 +47,25 @@ public class GameManager : MonoBehaviour
     void UpdateCursorPosition()
     {
         myCursorGameObject.transform.position = Input.mousePosition;
+    }
+
+    public void OnPressStart()
+    {
+        myMainScreen.SetActive(false);
+        myLevelSelectScreen.SetActive(true);
+
+    }
+
+    public void OnSelectLevel(int aLevelNumber)
+    {
+        if (myUnlockedLevelIndex <= aLevelNumber)
+        {
+            SceneManager.LoadScene(aLevelNumber);
+        }
+    }
+
+    public void OnPressQuit()
+    {
+        Application.Quit();
     }
 }
